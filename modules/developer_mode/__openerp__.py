ae0f2cdfdc44b3b3c61c0ed61ac4e063bfeff195
Ivan <ivkond@gmail.com>
2013-08-22 21:47:42 +0400

    [+] Собственно сам модуль. Реализовать настройку дефолтного фильтра через общее меню настроек, что бы можно было задавать шаблон поиска модулей. Добавить всплывающее окно с настройками модуля, в котором можно выбирать фильтр и напрямую обновить несколько последних модулей.

---
+++ modules/developer_mode/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Developer mode',
+    'version': '1.0',
+    'category': 'Development',
+    'author': 'Transparent Technologies',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'update_xml': ['developer_mode.xml'],
+    'installable': True,
+    'auto_install': False,
+    'description': '''
+    '''
+}
